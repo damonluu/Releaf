8440424038f3e0bcb6bda10d94e11dcb8b8a6bd1
Damon Luu <luudamon1996@gmail.com>
2018-01-19 15:54:58 -0800

    basic server with socket chat

---
+++ server.js
@@ -0,0 +1,77 @@
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+const express = require('express');
+const app = express();
+
+app.listen(5000);
+app.use(
+	express.static(__dirname + '/public', {
+		extensions: ['html']
+	})
+);
+
+// Connect to mongodb
+var url = 'mongodb://localhost:27017/mongochat';
+mongo.connect(url, function(err, db) {
+	if (err) {
+		throw err;
+	}
+	console.log('Connected to MongoDB...');
+
+	// Connect to Socket.io
+	client.on('connection', function(socket) {
+    const myDb = db.db('mongochat');
+    // myDb.collection('chats').insertOne({});
+
+		let chat = myDb.collection('chats');
+
+		// Create function to send status
+		sendStatus = function(s) {
+			socket.emit('status', s);
+		};
+
+		// Get chats from mongo collection
+		chat
+			.find()
+			.limit(100)
+			.sort({ _id: 1 })
+			.toArray(function(err, res) {
+				if (err) {
+					throw err;
+				}
+
+				// Emit the messages
+				socket.emit('output', res);
+			});
+
+		// Handle input events
+		socket.on('input', function(data) {
+			let name = data.name;
+			let message = data.message;
+
+			if (name == '' || message == '') {
+				sendStatus('Please enter a Name and Message');
+			} else {
+				// Insert message into MongoDB
+				chat.insert({ name: name, message: message }, function() {
+					client.emit('output', [data]);
+
+					// Sent status object
+					sendStatus({
+						message: 'Message Sent',
+						clear: true
+					});
+				});
+			}
+		});
+
+		// Handle clearing of chats
+		socket.on('clear', function(data) {
+			// Remove all chats from collection
+			chat.remove({}, function() {
+				// Emit cleared
+				socket.emit('cleared');
+			});
+		});
+	});
+});
